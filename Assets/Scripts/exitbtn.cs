6484a60c86a9d038d866bb003bdfe1f902c7dcfa
ardo <ardx160@gmail.com>
2021-02-26 19:53:54 +0700

    released

---
+++ Assets/Scripts/exitbtn.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class exitbtn : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        //Application.Quit();
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void BtnExit()
+    {
+        //SSTools.ShowMessage("Exit", SSTools.Position.bottom, SSTools.Time.oneSecond);
+        Application.Quit();
+    }
+}
